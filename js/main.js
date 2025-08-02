c89a6ab040628ce12a271cc74d8b59173d1f3b9d
cjw04 <93749597+cjw0417@users.noreply.github.com>
2025-08-02 19:50:03 +0900

    포트폴리오 업데이트
    포트폴리오 업데이트

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper-container', {
+  direction: 'vertical',
+  slidesPerView: 1,
+  mousewheel: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  speed: 800,
+});
